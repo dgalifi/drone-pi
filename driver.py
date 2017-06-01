f4d0d0ce74526a0c21ee8daf495ff5e3c0cad4c9
EUROPE\davideg <davidegalifi@gmail.com>
2017-06-01 13:43:30 +0100

    first commit

---
+++ driver.py
@@ -0,0 +1,203 @@
+import time
+import pigpio
+
+class driver:
+
+    def __init__(self,pi, M1, M2, M3, M4):
+        self.M1 = M1
+        self.M2 = M2
+        self.M3 = M3
+        self.M4 = M4
+        self.pi = pi
+        self.pulses = [0,0,0,0]
+        self.abs_speed = 0
+        
+        # f b r l
+        self.trims = [0,0,0,0]
+        self.pitches = [0,0,0,0]
+
+        self.MIN = 1000
+        self.MAX = 2000
+
+    def initialise(self):
+        
+        self.pi.set_PWM_frequency(self.M1, 1000)
+        self.pi.set_PWM_frequency(self.M2, 1000)
+        self.pi.set_PWM_frequency(self.M3, 1000)
+        self.pi.set_PWM_frequency(self.M4, 1000)
+        self.pi.set_PWM_range(self.M1, 1000)
+        self.pi.set_PWM_range(self.M2, 1000)
+        self.pi.set_PWM_range(self.M3, 1000)
+        self.pi.set_PWM_range(self.M4, 1000)
+
+        self.starting = 1
+        self.set_speed(0)
+
+        print("initialisation...")    
+        input("disconnect power, press enter to continue")
+        
+        self.set_speed(2000)
+        input("connect battery, then press enter when ready")
+        self.set_speed(self.MIN)
+        
+        print("ready")
+        
+        input("press enter to start")
+
+
+    # SET SPEED
+    # set speed of all the motors to the same value
+    def set_speed(self, pulse):
+        self.abs_speed = pulse
+        
+        self.pi.set_servo_pulsewidth(self.M1, self.abs_speed)
+        self.pi.set_servo_pulsewidth(self.M2, self.abs_speed)
+        self.pi.set_servo_pulsewidth(self.M3, self.abs_speed)
+        self.pi.set_servo_pulsewidth(self.M4, self.abs_speed)
+
+    def set_pitch_forward(self, value):
+        self.pi.set_servo_pulsewidth(self.M3, self.getSpeed(self.M3) + value)
+        self.pi.set_servo_pulsewidth(self.M4, self.getSpeed(self.M4) + value)
+        self.pitches[0] = value
+        
+    def set_pitch_back(self, value):
+        self.pi.set_servo_pulsewidth(self.M1, self.getSpeed(self.M1) + value)
+        self.pi.set_servo_pulsewidth(self.M2, self.getSpeed(self.M2) + value)
+        self.pitches[1] = value
+        
+    def set_pitch_right(self, value):
+        self.pi.set_servo_pulsewidth(self.M1, self.getSpeed(self.M1) + value)
+        self.pi.set_servo_pulsewidth(self.M4, self.getSpeed(self.M4) + value)        
+        self.pitches[2] = value
+        
+    def set_pitch_left(self, value):
+        self.pi.set_servo_pulsewidth(self.M2, self.getSpeed(self.M2) + value)
+        self.pi.set_servo_pulsewidth(self.M3, self.getSpeed(self.M3) + value)
+        self.pitches[3] = value
+        
+    def getActualSpeed(self, motor):
+        if motor == self.M1:
+            return self.abs_speed + self.trims[1] + self.trims[2] + self.pitches[1] + self.pitches[2]
+        if motor == self.M2:
+            return self.abs_speed + self.trims[1] + self.trims[3] + self.pitches[1] + self.pitches[3]
+        if motor == self.M3:
+            return self.abs_speed + self.trims[0] + self.trims[3] + self.pitches[0] + self.pitches[3]
+        else:
+            return self.abs_speed + self.trims[0] + self.trims[2] + self.pitches[0] + self.pitches[2]
+
+    def updateMotorSpeed(motor):
+
+        speed = 0
+        if motor == self.M1:
+            speed = self.abs_speed + self.trims[1] + self.trims[2] + self.pitches[1] + self.pitches[2]
+        if motor == self.M2:
+            speed = self.abs_speed + self.trims[1] + self.trims[3] + self.pitches[1] + self.pitches[3]
+        if motor == self.M3:
+            speed = self.abs_speed + self.trims[0] + self.trims[3] + self.pitches[0] + self.pitches[3]
+        else:
+            speed = self.abs_speed + self.trims[0] + self.trims[2] + self.pitches[0] + self.pitches[2]
+
+         self.pi.set_servo_pulsewidth(motor, speed)
+
+    # THROTTLE UP
+    def inc_speed(self, inc):
+        
+        while inc > 0 :
+            if self.abs_speed + max(self.trims) < 2000:
+                self.abs_speed = self.abs_speed + 1
+
+                self.pi.set_servo_pulsewidth(self.M1, self.abs_speed)
+                self.pi.set_servo_pulsewidth(self.M2, self.abs_speed)
+                self.pi.set_servo_pulsewidth(self.M3, self.abs_speed)
+                self.pi.set_servo_pulsewidth(self.M4, self.abs_speed)
+                
+                time.sleep(0.01)
+                inc = inc - 1
+            else:
+                inc = 0
+    
+    # THROTTLE DOWN
+    def dec_speed(self, dec):
+        
+        while dec > 0:
+            self.abs_speed = self.abs_speed - 1
+            
+            if self.abs_speed < 1100:                
+                self.set_speed(1000)
+                dec = 0
+            else:
+                self.pi.set_servo_pulsewidth(self.M1, self.abs_speed)
+                self.pi.set_servo_pulsewidth(self.M2, self.abs_speed)
+                self.pi.set_servo_pulsewidth(self.M3, self.abs_speed)
+                self.pi.set_servo_pulsewidth(self.M4, self.abs_speed)
+
+                time.sleep(0.01)
+                dec = dec - 1
+    
+    # OFF
+    def off(self):
+        while min(self.pulses) > 1000:
+            self.dec_speed(50)
+            time.sleep(0.001)
+        self.set_speed(0)
+        print("off!")
+        self.pi.stop()
+
+    def trim_forward(self):
+
+        # check trim_back
+        if self.trims[1] > 0:
+            self.trims[1] = self.trims[1] - 2
+
+            self.pi.set_servo_pulsewidth(self.M1, self.getSpeed(self.M1))
+            self.pi.set_servo_pulsewidth(self.M2, self.getSpeed(self.M2))
+        else:
+            self.trims[0] = self.trims[0] + 2
+            
+            self.pi.set_servo_pulsewidth(self.M3, self.getSpeed(self.M3))
+            self.pi.set_servo_pulsewidth(self.M4, self.getSpeed(self.M4))
+        
+    def trim_back(self):
+
+        # check trim_forward
+        if self.trims[0] > 0:
+            self.trims[0] = self.trims[0] - 2
+
+            self.pi.set_servo_pulsewidth(self.M3, self.getSpeed(self.M3))
+            self.pi.set_servo_pulsewidth(self.M4, self.getSpeed(self.M4))
+        else:
+            self.trims[1] = self.trims[1] + 2
+            
+            self.pi.set_servo_pulsewidth(self.M1, self.getSpeed(self.M1))
+            self.pi.set_servo_pulsewidth(self.M2, self.getSpeed(self.M2))
+
+        #self.print_status()
+
+    def trim_right(self):
+
+        if self.trims[3] > 0:
+            self.trims[3] = self.trims[3] - 2
+            
+            self.pi.set_servo_pulsewidth(self.M2, self.getSpeed(self.M2))
+            self.pi.set_servo_pulsewidth(self.M3, self.getSpeed(self.M3))
+        else:
+            self.trims[2] = self.trims[2] + 2
+            
+            self.pi.set_servo_pulsewidth(self.M1, self.getSpeed(self.M1))
+            self.pi.set_servo_pulsewidth(self.M4, self.getSpeed(self.M4))
+
+        #self.print_status()
+
+    def trim_left(self):
+
+        if self.trims[2] > 0:
+            self.trims[2] = self.trims[2] - 2
+            
+            self.pi.set_servo_pulsewidth(self.M1, self.getSpeed(self.M1))
+            self.pi.set_servo_pulsewidth(self.M4, self.getSpeed(self.M4))
+        else:
+            self.trims[3] = self.trims[3] + 2
+            
+            self.pi.set_servo_pulsewidth(self.M2, self.getSpeed(self.M2))
+            self.pi.set_servo_pulsewidth(self.M3, self.getSpeed(self.M3))
+        
---
+++ drone.py
@@ -0,0 +1,124 @@
+import time
+import _thread
+import pigpio
+import keyboard
+from rotation import *
+from driver import *
+from pid import *
+
+M1 = 17
+M2 = 18
+M3 = 19
+M4 = 20
+
+pi = pigpio.pi()
+driver = driver(pi, M1,M2,M3,M4)
+pulses = 0
+stop = 0
+accelerometer = accelerometer()
+pid = pid(10)
+rotation = [0,0]
+calibration = [0, 0]
+
+gap = 0
+
+def get_inc(value):
+    return value
+
+def adjust(rotation):
+    x = rotation[0]
+    y = rotation[1]
+
+    pitch_x = pid.get(x)
+    pitch_y = pid.get(y)
+    
+    if x < 0 - gap:
+        driver.set_pitch_forward(0)
+        driver.set_pitch_back(pitch_x)
+    else:
+        driver.set_pitch_back(0)
+        
+    if x > 0 + gap:
+        driver.set_pitch_forward(pitch_x)
+        driver.set_pitch_back(0)
+    else:
+        driver.set_pitch_forward(0)
+        
+    if y > 0 + gap:
+        driver.set_pitch_left(pitch_y)
+        driver.set_pitch_right(0)
+    else: 
+        driver.set_pitch_left(0)
+        
+    if y < 0 - gap:
+        driver.set_pitch_left(0)
+        driver.set_pitch_right(pitch_y)
+    else:
+        driver.set_pitch_right(0)
+
+def check_orientation():
+        global rotation
+        orientation = accelerometer.get_orientation()
+        rotation = [orientation[0] - calibration[0] , orientation[1] - calibration [1]]
+        adjust(rotation)
+        
+def check_thread():
+    while stop == 0:
+        check_orientation()
+        print_status()        
+        time.sleep(0.1)
+        
+def print_status():
+    global rotation
+    print("---------------")
+    print("| " + str(driver.getSpeed(M1))  + " | " + str(driver.getSpeed(M2)) + " |")
+    print("---------------")
+    print("| " + str(driver.getSpeed(M4))  + " | " + str(driver.getSpeed(M3)) + " |")
+    print("---------------")
+    print(str(rotation))
+
+try:
+    calibration = accelerometer.calibration(1000)
+
+    # balance point
+    print(calibration)
+
+    # initialise driver
+    driver.initialise()
+    print_status()
+    
+    print_status()
+    
+    # check thread
+    _thread.start_new_thread(check_thread, ())
+
+    # main thread waiting for input 
+    while stop == 0:
+        key = str(keyboard.getKey())
+        
+        if key[6:] == "[A'": #up
+            driver.inc_speed(20)
+        elif key[6:] == "[B'": #down
+            driver.dec_speed(20)
+        elif key == "b'w'":
+            driver.trim_forward()
+        elif key == "b's'":
+            driver.trim_back()
+        elif key == "b'a'":
+            driver.trim_left()
+        elif key == "b'd'":
+            driver.trim_right()
+        else:
+            print(key)
+                        
+    print("end loop")
+    driver.off()
+
+
+except KeyboardInterrupt:
+    print("you hit ctrl-c")
+    driver.off()
+    
+#except:
+   # print("generic exception")
+   # driver.off()  
